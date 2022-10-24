5c53ab14f8330dea60ef397f1ad9be81e61bb4bb
Prateek Gupta <prateekgupta22195@gmail.com>
2022-10-25 01:42:59 +0530

    DriveFileDao moved to dao

--- app/src/main/java/com/pg/cloudcleaner/data/db/dao/DriveFileDao.kt
+++ app/src/main/java/com/pg/cloudcleaner/data/db/dao/DriveFileDao.kt
@@ -1,4 +1,4 @@
-package com.pg.cloudcleaner
+package com.pg.cloudcleaner.data.db.dao
 
 import androidx.room.Dao
 import androidx.room.Delete
--- app/src/main/java/com/pg/cloudcleaner/service/GoogleDriveApiService.kt
+++ app/src/main/java/com/pg/cloudcleaner/service/GoogleDriveApiService.kt
@@ -12,33 +12,33 @@ interface GoogleDriveApiService {
 
     @GET("/drive/v3/files")
     fun filesData(
-            @Header("Authorization") auth: String,
-            @Query("key") key: String,
-            @Query("fields") fields: String,
-            @Query("pageSize") pageSize: Int,
-            @Query("pageToken") pageToken: String? = null,
+        @Header("Authorization") auth: String,
+        @Query("key") key: String,
+        @Query("fields") fields: String,
+        @Query("pageSize") pageSize: Int,
+        @Query("pageToken") pageToken: String? = null,
 //        @Query("key") Key : String
     ): Call<DriveAPIResponse>
 
     @GET("/v1/mediaItems")
     fun getPhotos(
-            @Query("pageToken") pageToken: String? = null,
+        @Query("pageToken") pageToken: String? = null,
     ): Call<PhotoAPIResponse>
 
     @DELETE("/drive/v3/files/{fileId}")
     suspend fun deleteFile(
-            @Path("fileId") fileId: String,
-            @Header("Authorization") auth: String,
-            @Query("key") key: String,
+        @Path("fileId") fileId: String,
+        @Header("Authorization") auth: String,
+        @Query("key") key: String,
     ): Response<ResponseBody>
 }
 
 data class DriveAPIResponse(
-        @SerializedName("nextPageToken") val nextTokenPage: String?,
-        @SerializedName("files") val files: List<DriveFile>,
+    @SerializedName("nextPageToken") val nextTokenPage: String?,
+    @SerializedName("files") val files: List<DriveFile>,
 )
 
 data class PhotoAPIResponse(
-        @SerializedName("nextPageToken") val nextTokenPage: String?,
-        @SerializedName("mediaItems") val mediaItems: List<MediaItem>,
+    @SerializedName("nextPageToken") val nextTokenPage: String?,
+    @SerializedName("mediaItems") val mediaItems: List<MediaItem>,
 )
