cbd8e62643f6bd6f005aed2dfe2ac15735a8eab8
Manpreet Singh <manpreetsinghkainth25@gmail.com>
2024-11-09 15:27:18 +0530

    [Mp] added testing logs to FM, sent to sentry and crashlytics

--- app/src/main/java/com/pg/cloudcleaner/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/pg/cloudcleaner/presentation/ui/MainActivity.kt
@@ -11,28 +11,12 @@ import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
 import android.os.Environment
 import android.provider.Settings
-import android.util.Log
-import android.view.Gravity
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.FrameLayout
-import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.semantics.text
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.app.ActivityCompat
 import androidx.navigation.compose.rememberNavController
 import androidx.work.ExistingWorkPolicy
@@ -40,20 +24,11 @@ import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.OutOfQuotaPolicy
 import androidx.work.WorkManager
 import com.google.android.material.snackbar.Snackbar
-import com.google.firebase.Firebase
-import com.google.firebase.FirebaseApp
-import com.google.firebase.crashlytics.FirebaseCrashlytics
-import com.neatroots.fbcrashlogs_testapp.LoggingUtils
 import com.pg.cloudcleaner.BuildConfig
+import com.pg.cloudcleaner.R
 import com.pg.cloudcleaner.app.App
 import com.pg.cloudcleaner.app.CloudCleanerApp
 import com.pg.cloudcleaner.helper.ReadFileWorker
-import io.sentry.Sentry
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import timber.log.Timber
 
 
 @ExperimentalFoundationApi
@@ -65,74 +40,6 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        FirebaseApp.initializeApp(this)
-
-// Use LoggingUtils to log initialization to both Firebase and Sentry
-        LoggingUtils.logInitialization("MainActivity initialized successfully")
-        Timber.d("manual log successfully created")
-
-        Sentry.init { options ->
-            options.dsn =
-                "https://a7a1e5324f974f6a9bd88b1fb0b59302@o4505045496037376.ingest.sentry.io/4505045496954880"
-            options.environment = "production" // Or your desired environment
-        }
-
-        LoggingUtils.logManualMessage("MainActivity initialized successfully")
-        setContent {
-            ForceCrashButton()
-        }
-
-        val crashButton = Button(this).apply {
-            text = "Test Crash"
-            setOnClickListener {
-                Toast.makeText(context, "Crash log recorded, app will crash", Toast.LENGTH_SHORT).show()
-
-                LoggingUtils.logCrash("Force Crash triggered by user")
-                // Force a crash
-                throw RuntimeException("This is a forced crash!")
-
-            }
-        }
-
-        val crashButton2 = Button(this).apply {
-            text = "Exception log test"
-            setOnClickListener {
-                try {
-                    val result = 10 / 0 // This will throw an ArithmeticException
-                } catch (e: Exception) {
-                    LoggingUtils.logException(e)
-                    Toast.makeText(context, "Exception log recorded", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-
-// Create a FrameLayout to center the button
-        val frameLayout = FrameLayout(this).apply {
-            layoutParams = ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
-            )
-        }
-
-// Add the button to the FrameLayout
-        frameLayout.addView(crashButton, FrameLayout.LayoutParams(
-            FrameLayout.LayoutParams.WRAP_CONTENT,
-            FrameLayout.LayoutParams.WRAP_CONTENT,
-            Gravity.START
-        ))
-
-        frameLayout.addView(crashButton2, FrameLayout.LayoutParams(
-            FrameLayout.LayoutParams.WRAP_CONTENT,
-            FrameLayout.LayoutParams.WRAP_CONTENT,
-            Gravity.END
-        ))
-
-// Add the FrameLayout to the activity's content view
-        addContentView(frameLayout, ViewGroup.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.MATCH_PARENT
-        ))
-
         requestPermissionLauncher = registerForActivityResult(
             ActivityResultContracts.StartActivityForResult()
         ) { result: ActivityResult ->
@@ -151,37 +58,8 @@ class MainActivity : AppCompatActivity() {
             seekStoragePermission()
         } else {
             onStoragePermissionGranted()
-    }
-
-}
-
-    @Composable
-    private fun ForceCrashButton() {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            Button(
-                onClick = {
-                    // Log the crash details before forcing the crash
-                    LoggingUtils.logCrash("Force Crash triggered by user")
-
-                    // Force a crash
-                    throw RuntimeException("This is a forced crash!")
-                }
-            )
-
-            {
-                Text(text = "Force Crash")
-            }
         }
-    }
 
-    @Preview(showBackground = true)
-
-    @Composable
-    fun DefaultPreview() {
-        ForceCrashButton()
     }
 
     private fun isStoragePermissionGranted(): Boolean {
@@ -196,6 +74,7 @@ class MainActivity : AppCompatActivity() {
             result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED
         }
     }
+
 
     private fun seekStoragePermission() {
         if (SDK_INT >= Build.VERSION_CODES.R) {
@@ -224,6 +103,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
     private fun onStoragePermissionGranted() {
         WorkManager.getInstance(applicationContext).enqueueUniqueWork(
             "file reader",
@@ -237,6 +117,7 @@ class MainActivity : AppCompatActivity() {
             CloudCleanerApp()
         }
     }
+
 
     override fun onRequestPermissionsResult(
         requestCode: Int, permissions: Array<out String>, grantResults: IntArray
